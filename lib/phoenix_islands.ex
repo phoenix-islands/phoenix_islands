3218f7f507ded9f896039c959167ba79248522e4
Ethan Dang <tiencuong.dang@gmail.com>
2024-01-11 15:44:39 +0700

    init

---
+++ lib/phoenix_islands.ex
@@ -0,0 +1,18 @@
+defmodule PhoenixIslands do
+  @moduledoc """
+  Documentation for `PhoenixIslands`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> PhoenixIslands.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/phoenix_islands_test.exs
@@ -0,0 +1,8 @@
+defmodule PhoenixIslandsTest do
+  use ExUnit.Case
+  doctest PhoenixIslands
+
+  test "greets the world" do
+    assert PhoenixIslands.hello() == :world
+  end
+end
